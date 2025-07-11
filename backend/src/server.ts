635a9dc5662e2b1241220d268dda21928aeb5ac0
RiverStepp <riverstepp@gmail.com>
2025-07-11 19:30:07 -0400

    It lied and said there were no conflicts oops

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -4,19 +4,6 @@ import { env } from "./config/env";
 import { createApp } from "./app";
 import { ensureContainer } from "./services/azureBlob.service";
 
-const app = createApp();
-
-const key  = fs.readFileSync("certs/key.pem");
-const cert = fs.readFileSync("certs/cert.pem");
-
-https
-  .createServer({ key, cert }, app)
-  .listen(env.PORT, () =>
-    console.log(`API running at https://localhost:${env.PORT}`)
-  );
-
-
-
 (async () => {
 
     await ensureContainer();
