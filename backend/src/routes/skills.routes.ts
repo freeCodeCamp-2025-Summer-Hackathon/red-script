47578307e3f6465507f8c9b3b4b82ae407065fba
Aaron Lopez <95320154+Aaron-Lopez-Dev@users.noreply.github.com>
2025-07-14 22:06:12 -0400

    Updated Skills Endpoint

---
+++ backend/src/routes/skills.routes.ts
@@ -0,0 +1,8 @@
+import { Router } from "express";
+import skillsController from "../controllers/skills.controller"
+
+const router = Router();
+
+router.get('/', skillsController.getAllSkills) 
+
+export default router
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -2,44 +2,11 @@ import https from "https";
 import fs from "fs";
 import { env } from "./config/env";
 import { createApp } from "./app";
-import { PrismaClient } from "../generated/prisma";
-import express, { Request, Response } from "express";
+import routes from "./routes";
 
 const app = createApp();
-const key = fs.readFileSync("certs/key.pem");
+const key  = fs.readFileSync("certs/key.pem");
 const cert = fs.readFileSync("certs/cert.pem");
-const prisma = new PrismaClient();
-
-// ************************************************************************
-
-app.get('/getskills', async (req: Request, res: Response) => {
-  const allSkills = await prisma.skills.findMany({
-    select: {
-      guid: true,
-      name: true,
-    },
-  });
-  res.json(allSkills)
-})
-
-// ************************************************************************
-
-
-
-app.post('/postSkill', async (req: Request, res: Response) => {
-  const { name } = req.body
-  const postSkill = await prisma.skills.create({
-    data: {
-      name: name
-    },
-  })
-  res.status(201).json(postSkill)
-})
-
-app.delete('/deleteAll', async(req: Request, res: Response) =>{
-  const deleteAll = await prisma.skills.deleteMany({})
-  res.status(201)
-})
 
 
 https
