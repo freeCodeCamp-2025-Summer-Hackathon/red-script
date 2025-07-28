a86a8a27d1bc50a0a368dc3a6f91b740daea556f
EsraMantei <75040035+EsraMantei@users.noreply.github.com>
2025-07-28 08:48:22 -0400

    Update FormCreateAccount.vue
    comment out "use current location" link (not implemented) and Country field (counter-intuitively requires integer)
    add onSubmit function to emit registrationData

--- frontend/src/components/auth/components/FormCreateAccount.vue
+++ frontend/src/components/auth/components/FormCreateAccount.vue
@@ -1,5 +1,5 @@
 <template>
-  <form class="w-full max-w-md flex flex-col p-10 gap-4">
+  <form class="w-full max-w-md flex flex-col p-10 gap-4" @submit.prevent="onSubmit">
 
     <fieldset class="rounded-md">
       <legend class="text-sm font-medium text-gray-700 mb-4">
@@ -10,6 +10,7 @@
         <input
           name="name"
           type="text"
+          v-model="name"
           placeholder="Full name"
           required
           aria-label="enter full name"
@@ -19,6 +20,7 @@
         <input
           name="username"
           type="text"
+          v-model="username"
           placeholder="Username"
           required
           aria-label="create username"
@@ -28,6 +30,7 @@
         <input
           name="email"
           type="email"
+          v-model="email"
           placeholder="Email"
           required
           aria-label="create email"
@@ -36,6 +39,7 @@
         <input
           name="password"
           type="password"
+          v-model="password"
           placeholder="Password"
           required
           aria-label="create password"
@@ -48,19 +52,20 @@
     <fieldset class="rounded-md">
       <div class="flex items-center justify-between mb-4">
         <legend class="text-sm font-medium text-gray-700">Location</legend>
-        <button
+        <!-- <button
           type="button"
           class="text-xs text-blue-600 hover:text-blue-800 underline"
           @click="getCurrentLocation"
           aria-label="Click here to use current location from browser"
         >
           Use Current Location
-        </button>
+        </button> -->
       </div>
       <div class="flex flex-col gap-4">
         <input
           name="city"
           type="text"
+          v-model="city"
           placeholder="City"
           required
           aria-label="Enter city"
@@ -69,22 +74,26 @@
         <input
           name="state"
           type="text"
+          id="state"
+          v-model="state"
           placeholder="State"
           required
-          aria-label="Enter  state"
+          aria-label="Enter state"
           class="pl-4 pr-3 py-2 w-full rounded-md bg-gray-100 focus:outline-none focus:ring-2 focus:ring-blue-500"
         />
-        <input
+        <!-- <input
           name="country"
-          type="text"
+          type="number"
+          v-model="country"
           placeholder="Country"
           required
-          aria-label="Enter country"
+          aria-label="Enter country code"
           class="pl-4 pr-3 py-2 w-full rounded-md bg-gray-100 focus:outline-none focus:ring-2 focus:ring-blue-500"
-        />
+        /> -->
         <input
           name="zip"
           type="text"
+          v-model="zip"
           placeholder="ZIP"
           required
           aria-label="Enter zip"
@@ -112,5 +121,54 @@
   </form>
 </template>
 <script setup lang="ts">
+import {ref} from 'vue';
+// import {useToast} from 'vue-toastification';
+const name=ref('');
+const username=ref('');
+const email=ref('');
+const password=ref('');
+const city=ref('');
+const state=ref('');
+//const country=ref('');
+const zip=ref('');
+
+const emit = defineEmits(['toggleForm','registrationSubmitted'])
+
+
+// sample data:
+// const registrationInfo = ref({
+//   email: "test6@example.com",
+//   password: "superSecure123!",
+//   username: "JohnSmith6",
+//   fullName: "John Smith",
+
+//   timezoneOffset: -240, // for example, UTC-4
+//   zip: "44114",
+//   city: "Cleveland",
+//   state: "Ohio",
+//   country: 1,
+
+//   latitude: 41.4993,
+//   longitude: -81.6944
+// })
+// const toast = useToast();
+const onSubmit = () => {
+  const registrationData = {
+  fullName: name.value,
+  username: username.value,
+  email: email.value,
+  password: password.value,
+  city: city.value,
+  state: state.value,
+  country: 1,
+  zip: zip.value,
+  
+  //function to find next items is not implemented
+  timezoneOffset: 0,
+  latitude: 0,
+  longitude: 0,
+  };
+  emit('registrationSubmitted', registrationData)
+}
 const getCurrentLocation = () => {};
 </script>
