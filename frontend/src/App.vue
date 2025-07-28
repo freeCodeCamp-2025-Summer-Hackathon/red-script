2cb43079a3cb8bdc4c763f2df478841b9962f204
EsraMantei <75040035+EsraMantei@users.noreply.github.com>
2025-07-28 08:50:58 -0400

    Update App.vue
    listen for registrationSubmitted and post to database

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <LoginPage />
+    <LoginPage @registrationSubmitted="handleRegistrationSubmitted"/>
     <!-- <div>
       <h1>Users</h1>
       <ul>
@@ -16,6 +16,7 @@
 
 <script setup lang="ts">
 import { ref, onMounted } from "vue";
+// import { useToast } from 'vue-toastification';
 import LoginPage from "./pages/LoginPage.vue";
 interface User {
   id: number;
@@ -26,15 +27,118 @@ interface User {
 const users = ref<User[]>([]);
 const error = ref<string | null>(null);
 
-onMounted(async () => {
-  // try {
-  //   const res = await fetch("https://localhost:4000/api/users");
-  //   if (!res.ok) throw new Error(`Status ${res.status}`);
-  //   users.value = await res.json();
-  // } catch (e: any) {
-  //   error.value = e.message;
-  //   console.error(e);
+
+
+// const toast = useToast();
+
+// sample data
+// const registrationInfo = {
+//   email: "test6@example.com",
+//   password: "superSecure123!",
+//   username: "JohnSmith6",
+//   fullName: "John Smith",
+
+//   timezoneOffset: -240, // for example, UTC-4
+//   zip: "44114",
+//   city: "Cleveland",
+//   state: "Ohio",
+//   country: 1,
+
+//   latitude: 41.4993,
+//   longitude: -81.6944
+// }
+
+const handleRegistrationSubmitted = (registrationData: Object) =>{
+  try {
+  //console.log(registrationData)
+let token = '';
+fetch("https://localhost:4000/api/auth/register", {
+  method: "POST",
+  headers: {
+    "Content-Type": "application/json"
+  },
+  body: JSON.stringify(registrationData)
+
+})
+  .then(response => response.json())
+  .then(data => {token=data});
+
+  // if (registrationData.password.length<8) {
+  //   toast.error("Password must be at least 8 characters!")
   // }
+  // if (registrationData.username.length<3) {
+  //   toast.error("Username must be at least 3 characters!")
+  // }
+  
+  
+} catch (e: any) {
+    error.value = e.message;
+    console.error(e);
+    // toast.error("Registration Error!");
+  }
+}
+
+
+
+
+onMounted(async () => {
+  
+//   try {
+    
+      
+// //     const payload = {
+// //   email: "test@example.com",
+// //   password: "superSecure123!",
+// //   username: "JohnSmith",
+// //   fullName: "John Smith",
+
+// //   timezoneOffset: -240, // for example, UTC-4
+// //   zip: "44114",
+// //   city: "Cleveland",
+// //   state: "Ohio",
+// //   country: 1,
+
+// //   latitude: 41.4993,
+// //   longitude: -81.6944
+// // };
+// // let token = '';
+// // const res = fetch("https://localhost:4000/api/auth/register", {
+// //   method: "POST",
+// //   headers: {
+// //     "Content-Type": "application/json"
+// //   },
+// //   body: JSON.stringify(payload)
+
+// // })
+// //   .then(response => response.json())
+// //   .then(data => {token=data})
+// //   .catch(error => console.error("Error:", error));
+
+//   const loginpayload= {
+//     email: "test@example.com",
+//     password: "superSecure123!",
+//   }
+//   let token2=''
+//   fetch("https://localhost:4000/api/auth/login", {
+// method: "POST", 
+// headers: {
+//     "Content-Type": "application/json"
+//   },
+//   body: JSON.stringify(loginpayload),
+//   })
+//   .then(response => response.json())
+//   .then(data => {token2=data})
+//   .catch(error => console.error("Error:", error));
+//     // const res = await fetch("https://localhost:4000/api/auth/register");
+//     // if (!res.ok) throw new Error(Status ${res.status});
+//     // users.value = await res.json();
+    
+//     //toast.success("Login Successful!")
+
+//   } catch (e: any) {
+//     error.value = e.message;
+//     console.error(e);
+//   }
 });
 </script>
 
